2dfbc2ccf36a1a7bbfb777d2c7baa182938f6986
Grupoufv <37818948+Grupoufv@users.noreply.github.com>
2018-03-26 23:38:24 -0300

    Create blinkserial.ino

---
+++ blinkserial.ino
@@ -0,0 +1,69 @@
+int led=13;
+int situa = 0; 
+int tempo=100;
+float tempo2;
+String inputString = "";          
+boolean stringCompleta = false;
+char letra;
+void setup()
+{
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+Leitura();
+
+  if(inputString=="+" ){
+      tempo=tempo*2;
+    }
+    if(inputString=="-"){
+    tempo=tempo/2;
+    }
+  Serial.println(inputString);
+  Serial.println(tempo);
+  if((letra=="R")||(letra=="S")){
+  digitalWrite(led,HIGH);
+  delay(tempo);
+  digitalWrite(led,LOW);
+}
+if((letra=="I")){
+   digitalWrite(led,HIGH);
+  delay(tempo2);
+  digitalWrite(led,LOW);
+ 
+  }
+  digitalWrite(led,HIGH);
+  delay(tempo);
+  digitalWrite(led,LOW);
+  if(inputString=="S"){
+   inputString="";
+   tempo=100;
+   }
+  ResetString();
+  
+}
+void Leitura() {
+  while (Serial.available()) {
+     letra = (char)Serial.read();
+    inputString += letra;
+    if (letra == '\n') {
+      stringCompleta = true;
+    }
+  /*  if(letra=='I'){
+       tempo2= (float)Serial.read();
+      }
+    if(inputString=="R"){
+       tempo=(int)Serial.read();
+      
+      }*/
+    }
+  }
+
+void ResetString(){
+   if(inputString!="S"){
+   inputString="";
+   }
+}
